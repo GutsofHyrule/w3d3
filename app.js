a6352daebc4158ff6c72edcf8b26a3d2032cd7bf
Spencer <dresmanns@gmail.com>
2022-04-14 08:32:54 -0400

    work

---
+++ app.js
@@ -0,0 +1,23 @@
+var a = 10;
+var b = 20;
+var c = 30;
+var d = 40;
+var e = 50;
+var f = 60;
+var g = 70;
+var h = 80;
+var i = 90;
+var j = 100;
+
+console.log(a + b);
+console.log(c + d);
+console.log(a - b);
+console.log(c - d);
+console.log(j * i);
+console.log(e * g);
+console.log(g / h);
+console.log(f / h);
+console.log(a++);
+console.log(b++);
+console.log(c--);
+console.log(d--);
